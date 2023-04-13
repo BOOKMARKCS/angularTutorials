abb721426f008785d9f2c97ce3287beaaa9fa041
MARKMACHINE <NaymarkCK.44@gmail.com>
2023-04-13 16:15:46 +0700

    add component page

---
+++ src/app/components/Pages/understanding-angular/understanding-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-understanding-angular',
+  templateUrl: './understanding-angular.component.html',
+  styleUrls: ['./understanding-angular.component.sass']
+})
+export class UnderstandingAngularComponent {
+
+}
